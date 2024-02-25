1f46f11b839451f28d7eba6adb176d7a4eb175b1
Lukáš Kraus <108264231+Darvener@users.noreply.github.com>
2024-02-25 19:27:41 +0100

    Add files via upload

---
+++ 4.py
@@ -0,0 +1,42 @@
+#proměnné
+def find(sekvence):
+    n = len(sekvence)
+    intervaly = []
+    for i in range(n):
+        for j in range(i + 1, n + 1):
+            if j - i >= 2:
+                interval = sekvence[i:j]
+                intervaly.append((i, j, sum(interval)))
+    return intervaly
+
+def cnt_rovno(intervaly):
+    rovno = {}
+    for i in range(len(intervaly)):
+        for j in range(i + 1, len(intervaly)):
+            if intervaly[i][2] == intervaly[j][2]:
+                par = (intervaly[i][:2], intervaly[j][:2])
+                rovno[par] = intervaly[i][2]
+    return rovno
+
+#main
+def main():
+    sekvence = []
+    try:
+        while True:
+            cis = input("Zadej číslo: ")
+            sekvence.append(int(cis))
+    except EOFError:
+        pass
+    if len(sekvence) == 0:
+        print("Vstupní posloupnost je prázdná.")
+        return
+    elif len(sekvence) > 2000:
+        print("Vstupní posloupnost je příliš dlouhá (přes 2000 čísel).")
+        return
+    intervaly = find(sekvence)
+    rovno = cnt_rovno(intervaly)
+    print("Počet nalezených dvojic intervalů se stejným součtem:", len(rovno))
+
+
+if __name__ == "__main__":
+    main()
